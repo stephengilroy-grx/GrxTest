b8505cd749e76e07185614a967d003a3fe61e13b
Stephen Gilroy <sgilroy@goodrx.com>
2022-06-02 16:36:11 -0700

    move to github

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-      .package(url: "file:///Users/sgilroy/Documents/Development/GrxSubTest", .upToNextMinor(from: "0.0.0")),
+      .package(url: "https://github.com/stephengilroy-grx/GrxSubTest.git", .upToNextMinor(from: "0.0.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
